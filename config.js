0047d25635fa067b5e6458ed4b68e14c8560c990
Rick S <rssedivec@gmail.com>
2026-03-06 15:54:48 -0500

    chore: add staging config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Environment-specific configuration 
+// DO NOT merge this file between branches
+const SUPABASE_URL      = 'https://puoaiyezsreowpwxzxhj.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_1jCe57nM0bKPBBZiMbo27Q_3Ztm1gb2';
